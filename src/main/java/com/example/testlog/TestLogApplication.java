4fda182751199cbec5a75e19816834c3572484e7
zhiyongli <iamlizhiyong@outlook.com>
2022-11-02 15:04:51 +0800

    Initial commit

---
+++ src/main/java/com/example/testlog/TestLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.testlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testlog/TestLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
